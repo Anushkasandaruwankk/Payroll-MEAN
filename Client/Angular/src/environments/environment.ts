3e396afc8f7a1351574e21e68bd2fc971de14c8f
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-07-23 00:56:54 +0530

    co

---
+++ Client/Angular/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  emailAPI: 'http://XXXXXX.com/contact-form.php',
+  database: 'graphql' ,
+  social: {
+    fblink: 'https://www.facebook.com/elishconsulting',
+    linkedin: 'YYYYYYYY',
+    github: 'https://github.com/AmitXShukla',
+    emailid: 'info@elishconsulting.com'
+  },
+  socialAuthEnabled: false,
+  graphql: 'http://localhost:3000/graphql'
+};
